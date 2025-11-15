1546e2be94192a6359dc9da950b9b4c304f11846
oneubp <paulo.bueno@fatec.sp.gov.br>
2025-11-14 21:33:44 -0300

    Fix rotas dos alarmes

--- SafeTempApp/src/screens/SettingsScreen.tsx
+++ SafeTempApp/src/screens/SettingsScreen.tsx
@@ -11,40 +11,56 @@ export default function SettingsScreen() {
 
   const [alertasAtivos, setAlertasAtivos] = useState([]);
 
-
-
-useEffect(() => {
+  useEffect(() => {
     carregarAlertas();
-},  []);
+  }, []);
 
-const carregarAlertas = async () => {
+  const carregarAlertas = async () => {
     try {
-      
-    const response = await api.get('alerts/list');
-    setAlertasAtivos(response.data); 
+
+      const response = await api.get('alerts/list');
+      setAlertasAtivos(response.data); 
     } catch (error) {
       console.error("Erro ao carregar alertas:", error);
     }
   };
 
- 
-  const handleDesativar = (id) => {
-    console.log('Desativar alerta:', id);
   
+  const handleDesativar = async (id) => {
+    console.log('Desativando alerta:', id);
+    try {
+
+      await api.put(`alerts/disable/${id}`);
+      
+
+      carregarAlertas();
+
+    } catch (error) {
+      console.error("Erro ao desativar alerta:", error);
+    }
   };
 
-  const handleExcluir = (id) => {
-    console.log('Excluir alerta:', id);
- 
+
+  const handleExcluir = async (id) => {
+    console.log('Excluindo alerta:', id);
+    try {
+
+      await api.delete(`alerts/delete/${id}`);
+
+      carregarAlertas();
+
+    } catch (error) {
+      console.error("Erro ao excluir alerta:", error);
+    }
   };
 
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-         <StatusBar barStyle="light-content" />
-          
-                     <View style={styles.logoContainer}>
-                        <Logo source={stconfig} />
-                      </View>
+        <StatusBar barStyle="light-content" />
+        
+            <View style={styles.logoContainer}>
+                <Logo source={stconfig} />
+            </View>
 
       <AlertasModal
         alertas={alertasAtivos}
